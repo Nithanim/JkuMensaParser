8401e4d8f05a1cb746af00e337458869d37a42e8
Nithanim <nithanim@yahoo.de>
2015-05-03 14:35:13 +0200

    Removed Gson

--- src/main/java/me/nithanim/mensaapi/Test.java
+++ src/main/java/me/nithanim/mensaapi/Test.java
@@ -1,7 +1,5 @@
 package me.nithanim.mensaapi;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import java.io.IOException;
 import java.util.List;
 import me.nithanim.mensaapi.parser.MainFactory;
@@ -13,18 +11,8 @@ public class Test {
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("http://menu.mensen.at/index/print/locid/1").get();
         List<Menu> menus = MainFactory.newMain(doc);
-        printJson(menus);
-    }
-    
-    private static void printToString(List<Menu> menus) {
         for(Menu m : menus) {
             System.out.println(m.toString());
         }
     }
-    
-    private static void printJson(List<Menu> menus) {
-        Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(menus));
-    }
-    
 }
