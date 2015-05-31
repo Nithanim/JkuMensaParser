a600a30b3120609254ca7d890c1a051cb6a928fc
Nithanim <nithanim@yahoo.de>
2015-05-31 10:14:27 +0200

    Added choice test

--- src/test/java/me/nithanim/mensaparser/jku/JkuChoiceFactoryTest.java
+++ src/test/java/me/nithanim/mensaparser/jku/JkuChoiceFactoryTest.java
@@ -1,14 +1,62 @@
 package me.nithanim.mensaparser.jku;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import me.nithanim.mensaapi.Meal;
 import me.nithanim.mensaapi.Menu;
+import me.nithanim.mensaapi.Type;
 import me.nithanim.mensaparser.ParserTest;
+import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class JkuChoiceFactoryTest extends ParserTest {
     public JkuChoiceFactoryTest() {
         super("jku/choice");
+    }
+    
+    @Test
+    public void testParseOrdinaryChoice() throws Exception {
+        getSourceFactory().setFile("without-snack.htm");
+        List<Menu> actual = getFactory().newJkuChoice();
+        
+        List<Menu> expected = new ArrayList<Menu>();
+        expected.add(new Menu(Type.CHOICE, "Grill",
+                Arrays.asList(new Meal[] {
+                    new Meal("Mensa Burger", 460)
+                }), -1, 0, "2015-05-29", false));
+        expected.add(new Menu(Type.CHOICE, "SnackEintopf",
+                Arrays.asList(new Meal[] {
+                    new Meal("Gebackene Tintenfischringe mit Karotffelsalat und Sauce Tartare", 510),
+                }), -1, 0, "2015-05-29", false));
+        expected.add(new Menu(Type.CHOICE, "Pizza/Pasta",
+                Arrays.asList(new Meal[] {
+                    new Meal("Al Tonno", 270),
+                    new Meal("Spaghetti Bolognese/Broccolisauce", 280)
+                }), -1, 0, "2015-05-29", false));
+        expected.add(new Menu(Type.CHOICE, "Suppentopf",
+                Arrays.asList(new Meal[] {
+                    new Meal("Klare Gemüsesuppe mit Profiteroles", 170),
+                }), -1, 0, "2015-05-29", false));
+        expected.add(new Menu(Type.CHOICE, "Brainfood",
+                Arrays.asList(new Meal[] {
+                    new Meal("Penne Tricolore mit Spargelsauce", 490),
+                }), -1, 0, "2015-05-29", false));
+        expected.add(new Menu(Type.CHOICE, "Gemüsebuffet",
+                Arrays.asList(new Meal[] {
+                    new Meal("Erdäpfel (vegan), Reis (vegan), Mischgemüse (vegan), Karfiol-Käselaibchen, Ratatouille Gemüse (vegan), Kohlsprossen (vegan)", -2),
+                }), -1, 0, "2015-05-29", false));
+        expected.add(new Menu(Type.CHOICE, "Süßhaus",
+                Arrays.asList(new Meal[] {
+                    new Meal("Topfen-Marillenstrudel mit Vanillesauce und Fruchtcocktail 2,40/4,80 Euro", -2),
+                }), -1, 0, "2015-05-29", false));
+        
+        assertEquals(expected.get(0), actual.get(0));
+        assertEquals(7, actual.size());
+        for(Menu m : expected) {
+            assertThat(actual, CoreMatchers.hasItem(m));
+        }
     }
     
     @Test
